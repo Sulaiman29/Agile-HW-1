c8868a68f89449bcbccf2b87a0163e5d60de9be7
Zeshan <zeshanhyder731@gmail.com>
2024-09-29 18:14:11 +0300

    test cases update

--- yahtzee/test/yahtzee_test.exs
+++ yahtzee/test/yahtzee_test.exs
@@ -32,24 +32,20 @@ defmodule YahtzeeTest do
 
   # Four of a kind
 
-  test "Identify 'Four of a kind' with every face" do
+  test "Identify 'Four of a kind' with face 5 and different first die" do
     assert %{"Four of a kind": 24} = Yahtzee.score_lower([4, 5, 5, 5, 5])
   end
 
-  test "Identify 'Four of a kind' with face 1" do
-    assert %{"Four of a kind": 4} = Yahtzee.score_lower([1, 1, 1, 1, 2])
+  test "Identify 'Four of a kind' with every same face" do
+    assert %{"Four of a kind": 35} = Yahtzee.score_lower([7, 7, 7, 7, 7])
   end
 
-  test "Identify 'Four of a kind' with face 2" do
-    assert %{"Four of a kind": 10} = Yahtzee.score_lower([2, 2, 2, 2, 3])
-  end
-
-  test "Identify 'Four of a kind' with face 3" do
-    assert %{"Four of a kind": 16} = Yahtzee.score_lower([3, 3, 3, 3, 4])
+  test "Identify 'Four of a kind' with empty list" do
+    assert %{"Four of a kind": 0} = Yahtzee.score_lower([])
   end
 
   test "Identify 'Four of a kind' with no matching four" do
-    assert %{} = Yahtzee.score_lower([1, 2, 3, 4, 5])
+    assert %{"Four of a kind": 0} = Yahtzee.score_lower([1, 2, 3, 4, 5])
   end
 
 end
