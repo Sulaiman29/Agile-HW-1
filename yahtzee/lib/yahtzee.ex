72e9773e87d92ad4733d7754572d9725dea4f77a
Sulaiman Javed <sulaimanjaved2000@gmail.com>
2024-09-29 17:37:02 +0300

    Three of a kind - Pass

--- yahtzee/lib/yahtzee.ex
+++ yahtzee/lib/yahtzee.ex
@@ -1,23 +1,15 @@
 defmodule Yahtzee do
-  def hello do
-    :world
+
+  def score_lower(dice) do
+    if check_three(dice) do
+       %{:"Three of a kind" => Enum.sum(dice)}
+    else
+       %{:"Three of a kind" => 0}
+    end
   end
 
-  def score_lower([]), do: %{:"Three of a kind" => 0}
-  def score_lower([head | tail]) do
-    %{:"Three of a kind" => head + (score_lower(tail)[:"Three of a kind"])}
+  defp check_three(dice) do
+     Enum.any?(dice, fn x -> Enum.count(dice, fn y -> y == x end) >= 3 end)
   end
-
-#   def score_lower(dice) do
-#     if three_of_a_kind?(dice) do
-#       %{"Three of a kind" => Enum.sum(dice)}
-#     else
-#       %{"Three of a kind" => 0}
-#     end
-#   end
-
-#   defp three_of_a_kind?(dice) do
-#     Enum.any?(dice, fn x -> Enum.count(dice, fn y -> y == x end) >= 3 end)
-#   end
 
 end
