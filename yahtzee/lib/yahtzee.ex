69323a95a74258e6ec0712f88ea99e604a8d63b3
Zeshan <zeshanhyder731@gmail.com>
2024-09-29 17:24:51 +0300

    second commit

--- yahtzee/lib/yahtzee.ex
+++ yahtzee/lib/yahtzee.ex
@@ -8,6 +8,16 @@ defmodule Yahtzee do
     %{:"Three of a kind" => head + (score_lower(tail)[:"Three of a kind"])}
   end
 
-  # aloha mora sulaiman
+#   def score_lower(dice) do
+#     if three_of_a_kind?(dice) do
+#       %{"Three of a kind" => Enum.sum(dice)}
+#     else
+#       %{"Three of a kind" => 0}
+#     end
+#   end
+
+#   defp three_of_a_kind?(dice) do
+#     Enum.any?(dice, fn x -> Enum.count(dice, fn y -> y == x end) >= 3 end)
+#   end
 
 end
